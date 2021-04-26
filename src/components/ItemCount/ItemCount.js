d535c1c425f8241fbd9a7a637c198c3f2c4456a7
Juan Gervatovski <57418116+juanegerva@users.noreply.github.com>
2021-04-26 20:12:37 -0300

    Contador con boton

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const ItemCount = ({ initial, stock}) => {
+  const [counter, setCounter] = useState(initial);
+
+
+  const decremento = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
+  const incremento = () => {
+    if (counter < stock) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const cantidadAgregada = () => {
+      counter !== 0 ? console.log("AGREGADO") : console.log("no hay stock");
+  }
+
+  return (
+    <div style={{ textAlign: "center"}}>
+      <button onClick={decremento}>
+        -
+      </button>
+      <button className=""> {counter} </button>
+      <button className="" onClick={incremento}>
+        +
+      </button>
+      <div>
+        <button className="" onClick={cantidadAgregada}>
+          AGREGAR
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,3 +1,4 @@
+import ItemCount from "../ItemCount/ItemCount";
 
 function ItemListContainer({greeting}){
 
@@ -5,6 +6,7 @@ function ItemListContainer({greeting}){
   return (
     <div className="itemListContainer">
       <h1>{greeting}</h1>
+      <ItemCount initial={1} stock={10} />
     </div>
   );
 }
