93e9ceb8d30d478ccd8ab71637e840b58174787c
Juan Gervatovski <57418116+juanegerva@users.noreply.github.com>
2021-05-05 22:19:23 -0300

    Routing

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ItemCount from "../ItemCount/ItemCount";
 
-function ItemDetail({ name, image, description, stock, initial, price }) {
+function ItemDetail({ products, name, image, description, stock, initial, price }) {
+  
   return (
     <div style={{ textAlign: "center" }}>
       <div>
         <div style={{ width: "20rem" }}>
           <img
             variant="top"
-            src={image}
+            src={products.thumbnail}
             style={{ width: "20rem", height: "15rem" }}
           />
           <div>
-            <h3>{name}</h3>
+            hola
+            <h3>{products}</h3>
             <h5>{description}</h5>
             <h4>{price}</h4>
           </div>
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -2,74 +2,43 @@ import React, { useState, useEffect } from "react";
 import ItemDetail from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
 
-const products = [
-  {
-    id: 1,
-    nombre: "Infantería",
-    precio: "10",
-    imagen: "https://picsum.photos/seed/picsum/200/300",
-    description: "Alto soldado",
-    stock: 3,
-    initial: 1,
-    categoryId: "infanteria",
-  },
-  {
-    id: 2,
-    nombre: "Arquero",
-    precio: "20",
-    imagen: "https://picsum.photos/seed/picsum/200/300",
-    description: "Alto Arquero",
-    stock: 5,
-    initial: 2,
-    categoryId: "arqueria",
-  },
-  {
-    id: 3,
-    nombre: "Caballería",
-    precio: "30",
-    imagen: "https://picsum.photos/seed/picsum/200/300",
-    description: "Alto Caballero",
-    stock: 6,
-    initial: 1,
-    categoryId: "caballeria",
-  },
-];
-
-
-
 
 function ItemDetailContainer() {
-  const [item, setItem] = useState();
-  const { id } = useParams();
+  const [item, setItem] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams(); 
 
   
   useEffect(() => {
-    const promesa = new Promise((resolve, reject) => {
-      setTimeout(function () {
-        const i = products.find((products) => products.id == id);
-        resolve(i);
-      }, 3000);
-    });
-    promesa.then((result) => setItem(result));
-    promesa.catch((err) => console.log("Algo salio mal"));
-  }, [id]);
+    const url = "https://api.mercadolibre.com/sites/MLA/search?q=tornillo";
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        setItem(response.results);
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (id) {
+      const category = item.filter((product) => product.categoryId == id);
+      setItem(category);
+    } else {
+      setItem(item);
+    }
+  }, [id, item]);
+
+  
 
   return (
-    <div className="itemDetailContainer">
-      {item ? (
-        <ItemDetail
-          id={item.id}
-          name={item.nombre}
-          price={item.precio}
-          image={item.imagen}
-          description={item.description}
-          stock={item.stock}
-          initial={item.initial}
-        />
-      ) : (
-        <h2>Loading</h2>
-      )}
+     <div className="itemListContainer">
+      <h2>Hola</h2>
+      {loading ? <h2>Cargando...</h2> : <ItemDetail products={item}  />}
     </div>
+   
   );
 }
 
