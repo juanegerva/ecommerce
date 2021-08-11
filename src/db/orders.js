26e4955b47eec240cb213022c57f4c1f0b2e8316
Juan Gervatovski <57418116+juanegerva@users.noreply.github.com>
2021-08-11 00:05:19 -0300

    acomodo

--- src/db/orders.js
+++
@@ -1,8 +0,0 @@
-import { getFirestore } from './firebase'
-
-export const createOrder = (order) => {
-  const db = getFirestore()
-  const orders = db.collection('orders')
-
-  return orders.add(order)
-}
